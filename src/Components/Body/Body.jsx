78540d98ee2e135542382cc8c1258e863e8a659a
jakubstetz <hugo.jakub.stetz@gmail.com>
2025-04-14 10:50:38 -0700

    style: prune website content so that it's in a deployable form

--- src/Components/Body/Body.jsx
+++ src/Components/Body/Body.jsx
@@ -3,10 +3,10 @@ import "./Body.css";
 function Body() {
   return (
     <main>
-      <h2>Hi. I go by Jakub. This is my website.</h2>
+      <h2>Hello. I go by Jakub. This is my website.</h2>
       <div className="underline"></div>
-      <p>This website serves as an informational resource and point of contact for my coaching services. I'm a certified a transformational coach through the <a href="https://satvatove.com/">Satvatove Institute</a>.</p>
-      <p>Also, this website serves as portfolio for my projects in software development. I'm currently a commissioned Naval nuclear officer, but I'm transitioning into the tech industry in 2025.</p>
+      <p>This website is currently under construction, so there's not much here at the moment.</p>
+      <p>I'm in the currently in the process of revamping it. So come back in a while and see what's new. :{")"}</p>
     </main>
   );
 }
