9c1549dd79abe51ee7b9ddf9dae6c17aca2b3ec9
jakubstetz <hugo.jakub.stetz@gmail.com>
2025-04-16 10:45:01 -0700

    feat: add tagline and introductory text

--- src/Components/BannerTextContainer/BannerTextContainer.jsx
+++ src/Components/BannerTextContainer/BannerTextContainer.jsx
@@ -5,7 +5,10 @@ function BannerTextContainer() {
   return (
     <div id="banner-text-container">
       <h1 className="centered-text">Hugo Jakub Stetz</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus mollis nulla vitae lectus ultricies commodo. Suspendisse convallis sapien in nulla accumsan ultrices.</p>
+      <div id='intro-text'>
+        <p>Backend-focused software engineer, shaped by monastic living and the Navy's nuclear engineering program.</p>
+        <p>Welcome to my website.</p>
+      </div>
       <ButtonContainer />
     </div>
   );
