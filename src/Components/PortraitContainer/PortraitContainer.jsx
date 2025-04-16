ed04447164009f25171f7031c13f1a258d06e33c
jakubstetz <hugo.jakub.stetz@gmail.com>
2025-04-16 09:48:27 -0700

    feat: add portrait to front page

--- src/Components/PortraitContainer/PortraitContainer.jsx
+++ src/Components/PortraitContainer/PortraitContainer.jsx
@@ -1,11 +1,10 @@
 import "./PortraitContainer.css";
 import portrait from '../../assets/images/portrait.jpg'
 
-/*<img src={portrait} />*/
-
 function PortraitContainer() {
   return (
-    <div id="portrait-container" className="image-container">
+    <div className="image-container">
+      <img src={portrait} />
     </div>
   );
 }
