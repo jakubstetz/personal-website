ccec979f40bfe60951d8e1ceca9100bc7436746c
jakubstetz <hugo.jakub.stetz@gmail.com>
2025-04-15 23:56:42 -0700

    style: reinsert navigation bar with adjusted buttons

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
   return (
     <>
       <HeroBanner />
+      <Break />
       <Body />
       <Footer />
     </>
--- src/Components/Break/Break.jsx
+++ src/Components/Break/Break.jsx
@@ -4,9 +4,8 @@ function Break() {
   return (
     <nav>
       <p>Homepage</p>
-      <a href="#">About Me</a>
-      <a href="#">Coaching</a>
-      <a href="#">Coding</a>
+      <a>About Me</a>
+      <a>Projects</a>
     </nav>
   );
 }
