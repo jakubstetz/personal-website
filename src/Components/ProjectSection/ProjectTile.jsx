8c1b8c3b2fa4026c76591d98436c0d81cee7b4ea
jakubstetz <hugo.jakub.stetz@gmail.com>
2025-04-22 14:50:17 -0700

    refactor: refactor codebase for shared usage of Button component across all website sections

--- src/Components/ProjectSection/ProjectTile.jsx
+++ src/Components/ProjectSection/ProjectTile.jsx
@@ -1,6 +1,7 @@
 import "./ProjectSection.css";
+import Button from "../Button";
 
-function ProjectTile() {
+function ProjectTile({ repo_link, demo_link }) {
   return (
     <div className="project-tile">
       <h2>Project Title</h2>
@@ -9,9 +10,15 @@ function ProjectTile() {
         ipsum, faucibus at imperdiet lobortis.
       </p>
       <img />
-      <div className="project-tile-buttons">
-        <button>GitHub</button>
-        <button>Demo</button>
+      <div className="button-row">
+        <Button
+          text={"GitHub"}
+          clickHandler={() => (window.location.href = repo_link)}
+        />
+        <Button
+          text={"Demo"}
+          clickHandler={() => (window.location.href = demo_link)}
+        />
       </div>
     </div>
   );
