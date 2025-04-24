d97364741ba98b1d0cf109e8afee307f246bd411
jakubstetz <hugo.jakub.stetz@gmail.com>
2025-04-23 17:46:59 -0700

    feat: implement rendering of actual project data in each tile

--- src/Components/ProjectSection/ProjectTile.jsx
+++ src/Components/ProjectSection/ProjectTile.jsx
@@ -1,23 +1,31 @@
 import "./ProjectSection.css";
 import Button from "../Button";
 
-function ProjectTile({ repo_link, demo_link }) {
+function ProjectTile({
+  title,
+  description,
+  screenshotFilename,
+  repoLink,
+  demoLink,
+}) {
+  const imageSource = new URL(
+    `../../assets/images/project-screenshots/${screenshotFilename}`,
+    import.meta.url,
+  ).href;
+
   return (
     <div className="project-tile">
-      <h2>Project Title</h2>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nisl
-        ipsum, faucibus at imperdiet lobortis.
-      </p>
-      <img />
+      <h2>{title}</h2>
+      <p>{description}</p>
+      <img src={imageSource} />
       <div className="button-row">
         <Button
           text={"GitHub"}
-          clickHandler={() => (window.location.href = repo_link)}
+          clickHandler={() => (window.location.href = repoLink)}
         />
         <Button
           text={"Demo"}
-          clickHandler={() => (window.location.href = demo_link)}
+          clickHandler={() => (window.location.href = demoLink)}
         />
       </div>
     </div>
