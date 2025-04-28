7a72273b1974ed2da9741e259f09b785961331c6
jakubstetz <hugo.jakub.stetz@gmail.com>
2025-04-28 00:30:24 -0700

    feat: improve navlink effects for hover and current

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,3 +1,5 @@
+import { motion } from "motion/react";
+
 function Navbar({ setView, currentView }) {
   const isCurrent = (page) =>
     currentView === page ? "nav-link current" : "nav-link";
@@ -6,24 +8,30 @@ function Navbar({ setView, currentView }) {
     <div className="section-top">
       <h2 className="subtle-heading">Hugo Jakub Stetz</h2>
       <nav id="navbar">
-        <span
+        <motion.span
+          layout
           className={isCurrent("HomePage")}
           onClick={() => setView("HomePage")}
+          transition={{ duration: 0.4, ease: "easeInOut" }}
         >
           Home
-        </span>
-        <span
+        </motion.span>
+        <motion.span
+          layout
           className={isCurrent("AboutMe")}
           onClick={() => setView("AboutMe")}
+          transition={{ duration: 0.4, ease: "easeInOut" }}
         >
           About Me
-        </span>
-        <span
+        </motion.span>
+        <motion.span
+          layout
           className={isCurrent("Projects")}
           onClick={() => setView("Projects")}
+          transition={{ duration: 0.4, ease: "easeInOut" }}
         >
           Projects
-        </span>
+        </motion.span>
       </nav>
     </div>
   );
