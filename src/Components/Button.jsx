f5eb81e7cfc570e978b9ebf4bfd4b27a2b03976f
jakubstetz <hugo.jakub.stetz@gmail.com>
2025-04-28 22:44:21 -0700

    feat: update homepage external links to open in a new tab

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,4 +1,17 @@
-function Button({ text, clickHandler }) {
+function Button({ text, clickHandler, href }) {
+  if (href) {
+    return (
+      <a
+        href={href}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="button shadow"
+      >
+        {text}
+      </a>
+    );
+  }
+
   return (
     <button onClick={clickHandler} className="button shadow">
       {text}
--- src/Components/HomePage/ButtonContainer.jsx
+++ src/Components/HomePage/ButtonContainer.jsx
@@ -8,17 +8,10 @@ function ButtonContainer({ setView }) {
         <Button text={"Projects"} clickHandler={() => setView("Projects")} />
       </div>
       <div className="button-row">
-        <Button
-          text={"GitHub"}
-          clickHandler={() =>
-            (window.location.href = "https://github.com/jakubstetz")
-          }
-        />
+        <Button text={"GitHub"} href={"https://github.com/jakubstetz"} />
         <Button
           text={"LinkedIn"}
-          clickHandler={() =>
-            (window.location.href = "https://www.linkedin.com/in/jakub-stetz/")
-          }
+          href={"https://www.linkedin.com/in/jakub-stetz/"}
         />
       </div>
     </div>
