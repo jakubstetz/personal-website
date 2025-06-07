a5492ef2cae83e4224c427a7d38fefe2d6194972
jakubstetz <hugo.jakub.stetz@gmail.com>
2025-06-07 12:36:24 -0600

    minor: adjust wording of personal website project description

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -20,7 +20,7 @@ const projects = [
   {
     title: "Personal Portfolio Website",
     description:
-      "Single-page portfolio site for showcasing projects, background, and contact information. Built with React, styled with vanilla CSS, and animated with Motion.",
+      "Portfolio site for showcasing projects, background, and contact information. Built with React, styled with vanilla CSS, and animated with Motion.",
     screenshotFilename: "jakubstetz-dev.jpeg",
     repoLink: "https://github.com/jakubstetz/personal-website",
     demoLink: "https://jakubstetz.dev",
